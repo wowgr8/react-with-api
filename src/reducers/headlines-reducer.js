c28a46c76ba993774e29344e3923da5c47541193
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-03 13:00:29 -0700

    add request_headlines action

--- src/reducers/headlines-reducer.js
+++ src/reducers/headlines-reducer.js
@@ -1,9 +1,18 @@
-const defaultState = {
+import * as c from './../actions/ActionTypes';
+
+let initialState = {
   isLoading: false,
   headlines: [],
   error: null
 }
 
-export default (state = defaultState, action) => {
-  return state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case c.REQUEST_HEADLINES:
+      return Object.assign({}, state, {
+        isLoading: true
+      });
+    default:
+      return state;
+    }
 };
