f7e0b90d80a988d9cffa73393234c1cef13fcda6
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-03 11:09:53 -0700

    add class component with local state

---
+++ src/components/Headlines.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Headlines extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      headlines: []
+    };
+  }
+
+  render() {
+    ...
+  }
+}
+
+export default Headlines;
