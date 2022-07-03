ed0d45aacf60701caade4fb9c36ed0ab47296acd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-03 11:15:35 -0700

    add makeApiCall method that updates state and makes API call

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -10,6 +10,24 @@ class Headlines extends React.Component {
     };
   }
 
+  makeApiCall = () => {
+    fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+      .then(response => response.json())
+      .then(
+        (jsonifiedResponse) => {
+          this.setState({
+            isLoaded: true,
+            headlines: jsonifiedResponse.results
+          });
+        })
+        .catch((error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        });
+  }
+
   render() {
     ...
   }
