eedd67309567429367387ee028409bd939d12b3e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-03 11:50:15 -0700

    add conditional to render

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -10,6 +10,10 @@ class Headlines extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.makeApiCall()
+  }
+
   makeApiCall = () => {
     fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
       .then(response => response.json())
@@ -29,7 +33,26 @@ class Headlines extends React.Component {
   }
 
   render() {
-    ...
+    const { error, isLoaded, headlines } = this.state;
+    if (error) {
+      return <React.Fragment>Error: {error.message}</React.Fragment>;
+    } else if (!isLoaded) {
+      return <React.Fragment>Loading...</React.Fragment>;
+    } else {
+      return (
+        <React.Fragment>
+          <h1>Headlines</h1>
+          <ul>
+            {headlines.map((headline, index) =>
+              <li key={index}>
+                <h3>{headline.title}</h3>
+                <p>{headline.abstract}</p>
+              </li>
+            )}
+          </ul>
+        </React.Fragment>
+      );
+    }
   }
 }
 
