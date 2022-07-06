e25181737bf2b8eff2633701a6a4237a9ca47643
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-05 19:30:56 -0700

    add redux store to app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import reducer from './reducers/headlines-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
