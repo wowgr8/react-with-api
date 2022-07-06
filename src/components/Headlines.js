5b2d4d11ee92af742177537ab5b49817ea0cf29b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-05 20:46:16 -0700

    add updates to refactor component that now uses redux middleware and thunk to handle state

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -1,51 +1,40 @@
 import React from 'react';
+// We need to import connect from React Redux. This will allow us to wrap our component with an HOC that provides Redux functionality such as the dispatch() function.
+import { connect } from 'react-redux';
+// We also need to import our ApiCall as well.
+import { makeApiCall } from '../actions/index';
 
 class Headlines extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      headlines: []
-    };
+    super(props); // We remove this.state from the constructor. We're using Redux to fully handle our state now.
+    // We will remove this.state since we'll be using the Redux store to handle state.
+    // this.state = {
+    //   error: null,
+    //   isLoaded: false,
+    //   headlines: []
+    // };
   }
 
-  componentDidMount() {
-    this.makeApiCall()
+  componentDidMount() { // We still make our API call in componentDidMount(). However, now we're using dispatch() to trigger it. Note that we aren't dispatching any of our reducer actions in Headlines.js - that's all being done in our async action. 
+    //(There are plenty of use cases where we would dispatch reducer actions still - however, we just don't need to do so here.)
+    // Now we'll use dispatch() to make our API call.
+    const { dispatch } = this.props;
+    dispatch(makeApiCall());
   }
-
-  // Note: This method we previously wrote to update local state will be updated to dispatch actions with redux thunk. It will be moved into actions/index.js
-
-  // makeApiCall = () => {
-  //   fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
-  //     .then(response => response.json())
-  //     .then(
-  //       (jsonifiedResponse) => {
-  //         this.setState({
-  //           isLoaded: true,
-  //           headlines: jsonifiedResponse.results
-  //         });
-  //       })
-  //       .catch((error) => {
-  //         this.setState({
-  //           isLoaded: true,
-  //           error
-  //         });
-  //       });
-  // }
 
   render() {
-    const { error, isLoaded, headlines } = this.state;
+    // We deconstruct the mapped Redux properties from this.props.
+    const { error, isLoading, headlines } = this.props; // we deconstruct this.props. Our Redux state has been mapped to this.props because...
     if (error) {
       return <React.Fragment>Error: {error.message}</React.Fragment>;
-    } else if (!isLoaded) {
+    } else if (isLoading) {
       return <React.Fragment>Loading...</React.Fragment>;
     } else {
       return (
         <React.Fragment>
           <h1>Headlines</h1>
           <ul>
-            {headlines.map((headline, index) =>
+            {headlines.map((headline, index) => // One more thing to note - we use index with map to give each headline an id. While this works fine, we recommend using UUID to set up ids instead.
               <li key={index}>
                 <h3>{headline.title}</h3>
                 <p>{headline.abstract}</p>
@@ -58,4 +47,21 @@ class Headlines extends React.Component {
   }
 }
 
-export default Headlines;
+
+// We'll also need to add mapStateToProps() as well.
+// We've added mapStateToProps() to map isLoading, headlines and error from Redux state.
+const mapStateToProps = state => {
+  return {
+    headlines: state.headlines,
+    isLoading: state.isLoading,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps)(Headlines); // Finally, we use connect() to wrap our component in an HOC with Redux functionality.
+
+// Note: When the application is ran; The logged messages are coming from our custom middleware function. As we can see here, the original state is the default state before we make the API call.
+  // The current action is GET_HEADLINES_SUCCESS - which includes a payload of NYT headlines.
+  // Finally, the updated state shows that headlines now has all the NYT headlines from our payload.
+
+  // This is what Redux middleware does for us. We can interrupt a reducer action to use middleware before, during or after that reducer action.
